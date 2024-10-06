3ac91fdd236f56e952e86c26e51945f3461e9bf7
Nifazur Rahman <nifazurrahman@Nifazurs-MacBook-Pro.local>
2024-10-06 23:59:31 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily:{
+        fuck:['Lexend Deca', 'sans-serif'],
+      }
+    },
+  },
+  plugins: [],
+}
